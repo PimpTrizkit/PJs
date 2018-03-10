b0f09064f5cc7835de98be7646e3e74154220f5a
Pimp Trizkit <12415937+PimpTrizkit@users.noreply.github.com>
2018-03-09 21:21:13 -0500

    Update pTrim.js

--- pTrim.js
+++ pTrim.js
@@ -1,1 +1,1 @@
-const pTrim=(s)=>{var s=s.replace(/^\s\s*/,''),w=/\s/,i=s.length;while(w.test(s.charAt(--i)));return s.slice(0,i+1);}
+const pTrim=(s)=>{if(!s||typeof(s)!="string")return "";var s=s.replace(/^\s\s*/,''),w=/\s/,i=s.length;while(w.test(s.charAt(--i)));return s.slice(0,i+1);}
