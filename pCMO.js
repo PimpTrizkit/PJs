236ac8096ced0ca9343351d02b9774862e3fb301
Pimp Trizkit <12415937+PimpTrizkit@users.noreply.github.com>
2018-03-24 02:44:22 -0400

    Create pCMO.js

---
+++ pCMO.js
@@ -0,0 +1,15 @@
+const pCMO =(cO,cPF)=> {
+    pCMO.PFS = Symbol(),
+    pCMO.CO = typeof(cO)=="function" ? cO : ()=> new Object(),
+    pCMO.CPF = typeof(cPF)=="function" ?  cPF : false,
+    pCMO.P =()=> {
+        let x = pCMO.CO();
+        if (pCMO.CPF) x[pCMO.PFS] = pCMO.CPF;
+        return new Proxy(x,pCMO.H);
+    },
+    pCMO.H = {
+        get: (o,p)=> { if (typeof(p) == 'symbol') { if (p == Symbol.toPrimitive && typeof(o[pCMO.PFS]) == "function" ) return o[pCMO.PFS]; } else if ( !(p in o) ) o[p] = pCMO.P(); return o[p]; },
+        set: (o,p,n)=> { delete o[pCMO.PFS]; o[p]=n; return true; }
+    };
+    return pCMO.P();
+};
