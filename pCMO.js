d6745efcb4897120de6d2f09c471a19fddb70d9a
Pimp Trizkit <12415937+PimpTrizkit@users.noreply.github.com>
2018-03-24 19:06:07 -0400

    Update pCMO.js

--- pCMO.js
+++ pCMO.js
@@ -1,17 +1,17 @@
 const pCMO =(cO,cPF)=> {
-	let cmo = {
-		PFS:Symbol(),
-		CO:typeof(cO)=="function" ? cO : ()=>({}),
-		CPF:typeof(cPF)=="function" ?  cPF : false,
-		P: ()=> {
-			let x = cmo.CO();
-			if (cmo.CPF) x[cmo.PFS] = cmo.CPF;
-			return new Proxy(x,cmo.H);
-		},
-		H: {
-			get: (o,p)=> { if (typeof(p) == 'symbol') { if (p == Symbol.toPrimitive && typeof(o[cmo.PFS]) == "function" ) return o[cmo.PFS]; } else if ( !(p in o) ) o[p] = cmo.P(); return o[p]; },
-			set: (o,p,n)=> { delete o[cmo.PFS]; o[p]=n; return true; }
-		}
+    let cmo = {
+        PFS:Symbol(),
+	CO:typeof(cO)=="function" ? cO : ()=>({}),
+	CPF:typeof(cPF)=="function" ?  cPF : false,
+	P: ()=> {
+            let x = cmo.CO();
+	    if (cmo.CPF) x[cmo.PFS] = cmo.CPF;
+	    return new Proxy(x,cmo.H);
+	},
+        H: {
+	    get: (o,p)=> { if (typeof(p) == 'symbol') { if (p == Symbol.toPrimitive && typeof(o[cmo.PFS]) == "function" ) return o[cmo.PFS]; } else if ( !(p in o) ) o[p] = cmo.P(); return o[p]; },
+	    set: (o,p,n)=> { delete o[cmo.PFS]; o[p]=n; return true; }
+	}
     };
-	return cmo.P();
+    return cmo.P();
 };
